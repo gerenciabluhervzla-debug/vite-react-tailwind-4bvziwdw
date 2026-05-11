6bb579a2bce670e79c28a43fc8e84878f19974dd
gerenciabluhervzla-debug <gerenciabluhervzla@gmail.com>
2026-05-11 14:38:37 -0400

    tailwind 4.0

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
@@ -14,3 +14,4 @@ export default defineConfig({
     },
   },
 })
+
